8ed1d09a9f92cda0e345ca7a86b2d9ecc41021e7
D0nghwi <donghwi617@gmail.com>
2025-11-27 04:37:53 +0900

    test6

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
+    //헤더 Tailwind클래스 - <가로100%  높이56px  배경흰색  그림자  요소가로나열  세로가운데아이템정렬  좌우패딩24px>
     <header className="w-full h-14 bg-white shadow flex items-center justify-between px-6">
       <div className="flex items-center gap-2">
         <span className="font-bold text-xl text-blue-600">StudyNote</span>
