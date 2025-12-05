bb6e238d9c8ef101e770ada50415c3205da46f1a
D0nghwi <donghwi617@gmail.com>
2025-12-05 17:16:01 +0900

    refactor: Organize frontend folder structure
    Reorganize the frontend directory according to the project conventions. Establish base folders such as component, page, util, store, and interface to improve clarity and maintainability.

--- frontend/src/interface/MainLayout.jsx
+++ frontend/src/interface/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import NavBar from "../components/NavBar";
-import Sidebar from "../components/Sidebar";
+import NavBar from "../component/header/NavBar";
+import Sidebar from "../component/sidebar/Sidebar";
 
 // 메인 레이아웃 
 // 모든 페이지에서 공통으로 사용 - 네비게이션 및 사이드바
